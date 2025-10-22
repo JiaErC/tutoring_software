a73decf6f66945f0fad6ccca3124c934bc10be80
徐家畅 <xujiahe2361683135@outlook.com>
2025-10-22 16:58:34 +0800

    优化代码，优化界面显示

--- lib/pages/subjects/subjects_page.dart
+++ lib/pages/subjects/subjects_page.dart
@@ -11,7 +11,12 @@ class SubjectsPage extends StatefulWidget {
 
 class _SubjectsPageState extends State<SubjectsPage> {
   //判断是老师还是学生
-  bool isTeacher = true;
+  bool _isTeacher = true;
+  //判断是否横屏
+  bool _isLandscape = false;
+  //判断两个下拉菜单是否展开
+  bool _isPrimaryExpanded = false; //这个是中小学科目菜单
+  bool _isSecondaryExpanded = false; //这个是大学生和成年人科目菜单
 
   //在didChangeDependencies()中获取路由参数
   @override
@@ -20,7 +25,7 @@ class _SubjectsPageState extends State<SubjectsPage> {
     // 在didChangeDependencies中获取路由参数
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
-    isTeacher = args?['isTeacher'] ?? true;
+    _isTeacher = args?['isTeacher'] ?? true;
     // 如果参数改变需要更新UI，可以调用setState
     // setState(() {
     //   isTeacher = args?['isTeacher'] ?? true;
@@ -29,6 +34,8 @@ class _SubjectsPageState extends State<SubjectsPage> {
 
   @override
   Widget build(BuildContext context) {
+    _isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -36,304 +43,177 @@ class _SubjectsPageState extends State<SubjectsPage> {
           tooltip: "返回上一页",
           onPressed: () => Navigator.pop(context), // 返回上一级路由
         ),
-        title: isTeacher ? const Text("选择教学科目") : const Text("选择学习科目"),
+        title: _isTeacher ? const Text("选择教学科目") : const Text("选择学习科目"),
       ),
       backgroundColor: Colors.white,
-      body: MediaQuery.of(context).orientation == Orientation.landscape
+      body: _isLandscape
           ? Align(
               alignment: Alignment.center,
-              child: SizedBox(
-                width: 1000,
-                child: Row(
-                  children: [
-                    Expanded(flex: 1, child: Container(color: Colors.red)),
-                    Expanded(flex: 1, child: _majorSubjectGroups(context)),
-                    Expanded(flex: 2, child: Container(color: Colors.blue)),
-                  ],
-                ),
+              child: Row(
+                children: [
+                  Expanded(flex: 2, child: _majorSubjectGroups(context)),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue[50],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             )
           : Row(
               children: [
-                Expanded(flex: 1, child: Container(color: Colors.red)),
                 Expanded(flex: 1, child: _majorSubjectGroups(context)),
-                Expanded(flex: 1, child: Container(color: Colors.blue)),
+                Expanded(
+                  flex: 2,
+                  child: Container(color: Colors.lightBlue[50]),
+                ),
               ],
             ),
     );
   }
 
+  //封装按钮，横屏的时候的按钮和竖屏时候的按钮不一样
+  Widget _buildButton(String s, IconData i) {
+    return _isLandscape
+        ? GFButton(
+            onPressed: () => debugPrint("选择$s"),
+            icon: Icon(i, size: 18),
+            shape: GFButtonShape.square,
+            fullWidthButton: true,
+            text: s,
+            textStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              color: Colors.black87,
+            ),
+            type: GFButtonType.transparent,
+          )
+        : SizedBox(
+            height: 60,
+            child: GFButton(
+              onPressed: () => debugPrint("选择$s"),
+              icon: Icon(i, size: 23), // 增加图标大小
+              shape: GFButtonShape.square,
+              fullWidthButton: true,
+              text: s,
+              textStyle: TextStyle(
+                fontSize: 20, // 增加文字大小
+                fontWeight: FontWeight.w700,
+                color: Colors.black87,
+              ),
+              type: GFButtonType.transparent,
+              padding: EdgeInsets.all(12), // 添加内边距参数
+            ),
+          );
+  }
+
   //以下为选择科目组件
   //大科目菜单
   Widget _majorSubjectGroups(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.zero,
-      physics: AlwaysScrollableScrollPhysics(),
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: Column(
-          mainAxisSize: MainAxisSize.min, // 使Column只占用其子内容所需的空间
-          crossAxisAlignment: CrossAxisAlignment.start, // 内容左对齐
-          children: [
-            // 使用ExpansionTile创建下拉菜单
-            ExpansionTile(
-              title: const Text(
-                "小学和中学",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w800,
-                  color: Color.fromARGB(255, 25, 27, 31),
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // 使Column只占用其子内容所需的空间
+            crossAxisAlignment: CrossAxisAlignment.start, // 内容左对齐
+            children: [
+              // 使用ExpansionTile创建下拉菜单
+              ExpansionTile(
+                leading: const Icon(MdiIcons.tableChair),
+                textColor: Colors.deepPurpleAccent,
+                title: Text(
+                  _isLandscape?"小学和中学":"小学\n中学",
+                  style: _isLandscape
+                      ? TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w800,
+                          color: _isPrimaryExpanded?Colors.purple[900]:Colors.black87,
+                        )
+                      : TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800,
+                          color: _isPrimaryExpanded?Colors.purple[900]:Colors.black87,
+                        ),
                 ),
+                subtitle: Text(
+                  _isPrimaryExpanded?"":"适合中小学生未成年人，需要绑定家长",
+                  style: TextStyle(fontSize: 10, color: Colors.black45),
+                ),
+                // 控制初始是否展开
+                initiallyExpanded: _isPrimaryExpanded,
+                //回调函数，获取是否展开
+                onExpansionChanged: (bool expanded) {
+                  setState(() {
+                    _isPrimaryExpanded = expanded;
+                  });
+                },
+                // 下拉菜单中的内容
+                children: <Widget>[
+                  const SizedBox(height: 10),
+                  // 小学科目
+                  _buildButton("小学科目", MdiIcons.pencil),
+                  // 初中科目
+                  _buildButton("初中科目", MdiIcons.pen),
+                  // 高中科目
+                  _buildButton("高中科目", MdiIcons.bookOpenBlankVariant),
+                  //兴趣爱好
+                  _buildButton("兴趣爱好", MdiIcons.heartPulse),
+                  const SizedBox(height: 10),
+                ],
               ),
-              // 控制初始是否展开
-              initiallyExpanded: false,
-              // 下拉菜单中的内容
-              children: [
-                const SizedBox(height: 10),
-                // 小学科目
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: GFButton(
-                    onPressed: () => debugPrint("选择小学科目"),
-                    icon: Icon(MdiIcons.pencil, size: 16),
-                    shape: GFButtonShape.square,
-                    fullWidthButton: true,
-                    text: "小学科目",
-                    textStyle: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black87,
-                    ),
-                    type: GFButtonType.transparent,
-                  ),
+              ExpansionTile(
+                leading: const Icon(MdiIcons.accountSchool),
+                title: Text(
+                  "大学学习\n终身学习",
+                  style: _isLandscape
+                      ? TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w800,
+                          color: _isSecondaryExpanded?Colors.purple[900]:Colors.black87,
+                        )
+                      : TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800,
+                          color: _isSecondaryExpanded?Colors.purple[900]:Colors.black87,
+                        ),
                 ),
-                // 初中科目
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: GFButton(
-                    onPressed: () => debugPrint("选择高中科目"),
-                    icon: Icon(MdiIcons.pen, size: 16),
-                    shape: GFButtonShape.square,
-                    fullWidthButton: true,
-                    text: "初中科目",
-                    textStyle: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black87,
-                    ),
-                    type: GFButtonType.transparent,
-                  ),
+                subtitle: Text(
+                  _isSecondaryExpanded?"":"适合成年人，不需要绑定家长",
+                  style: TextStyle(fontSize: 10, color: Colors.black45),
                 ),
-                // 高中科目
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: GFButton(
-                    onPressed: () => debugPrint("选择高中科目"),
-                    icon: Icon(MdiIcons.bookOpenBlankVariant, size: 16),
-                    fullWidthButton: true,
-                    shape: GFButtonShape.square,
-                    text: "高中科目",
-                    textStyle: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black87,
-                    ),
-                    type: GFButtonType.transparent,
-                  ),
-                ),
-                // 兴趣爱好
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: GFButton(
-                    onPressed: () => debugPrint("选择兴趣爱好"),
-                    icon: Icon(MdiIcons.music, size: 16),
-                    shape: GFButtonShape.square,
-                    fullWidthButton: true,
-                    text: "兴趣爱好",
-                    textStyle: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black87,
-                    ),
-                    type: GFButtonType.transparent,
-                  ),
-                ),
-                const SizedBox(height: 10),
-              ],
-            ),
-            ExpansionTile(
-              title: const Text(
-                "大学学习\n终身学习",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w800,
-                  color: Color.fromARGB(255, 25, 27, 31),
-                ),
+                // 控制初始是否展开
+                initiallyExpanded: _isSecondaryExpanded,
+                onExpansionChanged: (bool expanded) {
+                  setState(() {
+                    _isSecondaryExpanded = expanded;
+                  });
+                },
+                // 下拉菜单中的内容
+                children: <Widget>[
+                  const SizedBox(height: 10),
+                  _buildButton("理科数学物理", MdiIcons.bookOpenVariant),
+                  _buildButton("IT互联网", MdiIcons.wifi),
+                  _buildButton("健康和锻炼", MdiIcons.dumbbell),
+                  _buildButton("外语学习", MdiIcons.abTesting),
+                  _buildButton("设计与艺术", MdiIcons.palette),
+                  _buildButton("工学技能", MdiIcons.wrench),
+                  _buildButton("商业管理", MdiIcons.briefcase),
+                  _buildButton("历史与文化", MdiIcons.bookOpen),
+                  _buildButton("环境科学", MdiIcons.leaf),
+                  _buildButton("创业与创新", MdiIcons.lightbulb),
+                  _buildButton("个人成长", MdiIcons.heart),
+                ],
               ),
-              // 控制初始是否展开
-              initiallyExpanded: false,
-              // 下拉菜单中的内容
-              children: [
-                const SizedBox(height: 10),
-                GFButton(
-                  onPressed: () => debugPrint("选择理科数学物理"),
-                  icon: Icon(MdiIcons.calculator, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "理科数学物理",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择IT互联网"),
-                  icon: Icon(MdiIcons.wifi, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "IT互联网",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择健康和锻炼"),
-                  icon: Icon(MdiIcons.dumbbell, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "健康和锻炼",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择外语学习"),
-                  icon: Icon(Icons.abc_sharp, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "外语学习",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择设计和艺术"),
-                  icon: Icon(MdiIcons.palette, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "设计与艺术",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择工学技能"),
-                  icon: Icon(MdiIcons.wrench, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "工学技能",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择商业管理"),
-                  icon: Icon(MdiIcons.briefcase, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "商业管理",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择心理学与教育"),
-                  icon: Icon(MdiIcons.brain, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "心理学与教育",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择历史与文化"),
-                  icon: Icon(MdiIcons.bookOpen, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "历史与文化",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择环境科学"),
-                  icon: Icon(MdiIcons.leaf, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "环境科学",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择创业与创新"),
-                  icon: Icon(MdiIcons.lightbulb, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "创业与创新",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-                GFButton(
-                  onPressed: () => debugPrint("选择个人成长"),
-                  icon: Icon(MdiIcons.heart, size: 16),
-                  shape: GFButtonShape.square,
-                  fullWidthButton: true,
-                  text: "个人成长",
-                  textStyle: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black87,
-                  ),
-                  type: GFButtonType.transparent,
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
