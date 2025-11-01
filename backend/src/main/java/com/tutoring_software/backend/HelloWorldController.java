55eac22bfbadc331fe14f9e16993d17b7a8bd694
jiaerc <xujiahe2361683135@outlook.com>
2025-11-01 13:02:53 +0800

    Springboot打印helloworld

--- backend/src/main/java/com/tutoring_software/backend/HelloWorldController.java
+++ backend/src/main/java/com/tutoring_software/backend/HelloWorldController.java
@@ -1,4 +1,12 @@
 package com.tutoring_software.backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
 }
