fceb983d3957e35dc68b258f18f5e3e0766fbe1b
jiaerc <xujiahe2361683135@outlook.com>
2025-11-04 22:57:14 +0800

    成功关联后端和数据库

--- backend/src/main/java/com/tutoring_software/backend/uid_management/mapper/PhoneNumberUidMapper.java
+++ backend/src/main/java/com/tutoring_software/backend/uid_management/mapper/PhoneNumberUidMapper.java
@@ -4,10 +4,13 @@ package com.tutoring_software.backend.uid_management.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tutoring_software.backend.uid_management.entity.PhoneNumberUid;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface PhoneNumberUidMapper extends BaseMapper<PhoneNumberUid> {
     // 可以添加自定义SQL方法
+    @Select("SELECT * FROM phone_number_uid WHERE phone_number = #{phoneNumber}")
     PhoneNumberUid selectByPhoneNumber(String phoneNumber);
+    @Select("SELECT * FROM phone_number_uid WHERE uid = #{uid}")
     PhoneNumberUid selectByUid(Long uid);
 }
