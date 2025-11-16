06f0d32d66e09beb357b991d064be90f739ae4b5
JIAERC <xujiahe2361683135@outlook.com>
2025-11-16 22:50:14 +0800

    添加json辅助方法

---
+++ lib/bean/data_process/json_process.dart
@@ -0,0 +1,14 @@
+// 添加深拷贝辅助方法
+Map<String, dynamic> _deepCopyMap(Map<String, dynamic> source) {
+  final result = <String, dynamic>{};
+  source.forEach((key, value) {
+    if (value is Map<String, dynamic>) {
+      result[key] = _deepCopyMap(value);
+    } else if (value is List) {
+      result[key] = List.from(value);
+    } else {
+      result[key] = value;
+    }
+  });
+  return result;
+}
