e76590e02d1f2b6667ab8a4e88a0a880b8192b77
jiaerc <xujiahe2361683135@outlook.com>
2025-11-19 13:07:08 +0800

    创建控制类的本体类

---
+++ lib/modules/comment/comment.dart
@@ -0,0 +1,10 @@
+
+
+class Comment{
+  String studentUid = "";
+  String teacherUid = "";
+  String comment = "";
+  String timeAt = "";
+  int rating = 0;
+  String subject = "";
+}
---
+++ lib/modules/comment/comment_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+import 'package:tutoring_software/modules/api/api_settings.dart';
+
+part 'comment_controller.g.dart';
+
+class CommentController = _CommentController with _$CommentController;
+
+abstract class _CommentController with Store {
+  @observable
+  String comment = '';
+}
