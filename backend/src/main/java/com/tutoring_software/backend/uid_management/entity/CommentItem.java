016a4d74910128a61e670a7a02dea99a6c31a1f6
JIAERC <xujiahe2361683135@outlook.com>
2025-11-19 14:26:02 +0800

    修复错误，发布评论成功

--- backend/src/main/java/com/tutoring_software/backend/uid_management/entity/CommentItem.java
+++ backend/src/main/java/com/tutoring_software/backend/uid_management/entity/CommentItem.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat; 
 
 @TableName("comments")
 public class CommentItem {
@@ -30,6 +31,7 @@ public class CommentItem {
     @TableField("rating")
     private String rating;
     @TableField("created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
     @TableField("subject")
     private String subject;
